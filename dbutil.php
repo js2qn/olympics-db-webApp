9ff8c234b1f11038b7bc98910ae4ffd9f04675c0
Jeffrey Wang <kingjeff2115@gmail.com>
2021-05-02 15:27:25 -0400

    Update dbutil.php

--- dbutil.php
+++ dbutil.php
@@ -1,10 +1,17 @@
 
 <?php
+$username = '';
+if($_SESSION["usertype"] == 1) {
+	$username = 'jg6sa_a';
+}
+else {
+	$username = 'jg6sa_b';
+}
 class DbUtil{
-        public static $loginUser = "jg6sa";
+        public static $loginUser = $username;
         public static $loginPass = "CS4750group20!";
         public static $host = "usersrv01.cs.virginia.edu"; // DB Host
-        public static $schema = "jg6sa_olympics"; // DB Schema
+        public static $schema = "jg6sa"; // DB Schema
 
         public static function loginConnection(){
                 $db = new mysqli(DbUtil::$host, DbUtil::$loginUser, DbUtil::$loginPass, DbUtil::$schema);
