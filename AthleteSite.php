7507f81db3c19a136bafffae4abf8a70a6d29b76
js2qn <54920799+js2qn@users.noreply.github.com>
2021-05-01 22:12:51 -0400

    Update AthleteSite.php
    added update

--- AthleteSite.php
+++ AthleteSite.php
@@ -4,10 +4,23 @@
 
 $ID = mysqli_real_escape_string($con, $_GET['ID']);
 
-$query ="SELECT Name FROM athlete NATURAL JOIN AthleteParticipates WHERE ID = '$ID' LIMIT 1";
-$result = mysqli_query($con, $query);
-$row = mysqli_fetch_array($result);
-// Echo page content
-echo $row['Name'];
+if (mysqli_connect_errno())
+ {
+ echo "Failed to connect to MySQL: " . mysqli_connect_error();
+ }
+ // Form the SQL query (an INSERT query)
+ $sql="UPDATE athlete SET clickCnt = clickCnt + 1 WHERE ID = $ID";
+ $query ="SELECT Name FROM athlete NATURAL JOIN AthleteParticipates WHERE ID = '$ID' LIMIT 1";
+ $result = mysqli_query($con, $query);
+ $row = mysqli_fetch_array($result);
+ // Echo page content
+ echo $row['Name'];
+
+ if (!mysqli_query($con,$sql))
+ {
+ die('Error: ' . mysqli_error($con));
+ }
+ echo " -- was clicked"; // Output to user
+
 mysqli_close($con);
 ?>
