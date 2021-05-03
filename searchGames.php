81dbbc6fb88525da49c1a74e596255b2fbf40ff8
jw4tp <kingjeff2115@gmail.com>
2021-05-03 17:13:21 -0400

    minor fix with prepare statement bindings

--- searchGames.php
+++ searchGames.php
@@ -17,7 +17,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 
         if($stmt->prepare("select * from game_name where Games like ?") or die(mysqli_error($db))) {
                 $searchString = '%' . $_GET['gamesOfInterest'] . '%';
-                $stmt->bind_param(s, $searchString);
+                $stmt->bind_param('s', $searchString);
                 $stmt->execute();
                 $stmt->bind_result($Games, $Year, $Season);
                 echo "<table class='table table-hover table-bordered' border=1><th>Games</th><th>Year</th><th>Season</th><th>Details</th>\n";
--- searchTeams.php
+++ searchTeams.php
@@ -15,7 +15,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 
         if($stmt->prepare("select * from team where Team like ? OR NOC like ?") or die(mysqli_error($db))) {
                 $searchString = '%' . $_GET['teamNameORnoc'] . '%';
-                $stmt->bind_param(ss, $searchString, $searchString);
+                $stmt->bind_param('ss', $searchString, $searchString);
                 $stmt->execute();
                 $stmt->bind_result($Team, $NOC);
                 echo "<table class='table table-hover table-bordered' border=1><th>Team</th><th>NOC</th><th>Details</th>\n";
